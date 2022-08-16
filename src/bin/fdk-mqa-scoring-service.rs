9a5a145bea22ef45e69fd8e50b6d60a3c530d044
Amund Tenstad <github@amund.io>
2022-08-16 08:10:31 +0200

    feat: metrics (#44)

--- src/bin/fdk-mqa-scoring-service.rs
+++ src/bin/fdk-mqa-scoring-service.rs
@@ -1,7 +1,36 @@
-use fdk_mqa_scoring_service::kafka::{
-    create_sr_settings, run_async_processor, BROKERS, INPUT_TOPIC, SCHEMA_REGISTRY, SCORING_API_URL,
+use actix_web::{get, App, HttpServer, Responder};
+use fdk_mqa_scoring_service::{
+    kafka::{
+        create_sr_settings, run_async_processor, BROKERS, INPUT_TOPIC, SCHEMA_REGISTRY,
+        SCORING_API_URL,
+    },
+    metrics::{get_metrics, register_metrics},
 };
-use futures::stream::{FuturesUnordered, StreamExt};
+use futures::{
+    stream::{FuturesUnordered, StreamExt},
+    FutureExt,
+};
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    "pong"
+}
+
+#[get("/ready")]
+async fn ready() -> impl Responder {
+    "ok"
+}
+
+#[get("/metrics")]
+async fn metrics() -> impl Responder {
+    match get_metrics() {
+        Ok(metrics) => metrics,
+        Err(e) => {
+            tracing::error!(error = e.to_string(), "unable to gather metrics");
+            "".to_string()
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -11,6 +40,8 @@ async fn main() {
         .with_target(false)
         .with_current_span(false)
         .init();
+
+    register_metrics();
 
     tracing::info!(
         brokers = BROKERS.to_string(),
@@ -25,8 +56,20 @@ async fn main() {
         std::process::exit(1);
     });
 
+    let http_server = tokio::spawn(
+        HttpServer::new(|| App::new().service(ping).service(ready).service(metrics))
+            .bind(("0.0.0.0", 8000))
+            .unwrap_or_else(|e| {
+                tracing::error!(error = e.to_string(), "metrics server error");
+                std::process::exit(1);
+            })
+            .run()
+            .map(|f| f.map_err(|e| e.into())),
+    );
+
     (0..4)
         .map(|i| tokio::spawn(run_async_processor(i, sr_settings.clone())))
+        .chain(std::iter::once(http_server))
         .collect::<FuturesUnordered<_>>()
         .for_each(|result| async {
             result
@@ -39,5 +82,5 @@ async fn main() {
                     std::process::exit(1);
                 });
         })
-        .await
+        .await;
 }
